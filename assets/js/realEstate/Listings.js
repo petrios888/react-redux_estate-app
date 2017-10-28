6803cce6d690baa4adb2c92f16a80a6431c6d8c5
petrios888 <appeter22@yahoo.com>
2017-10-28 17:50:44 -0400

    gamma.3

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -10,7 +10,72 @@ export default class Listings extends Component {
   }
   render () {
     return (<section className="listings">
-    Listings
+    <section className="search-area">
+    <input type="text" name="search"/>
+    </section>
+
+    <section className="sortby">
+    <div>
+      390 results found
+    </div>
+    <div className="sort-options">
+      <select name="sortby" className="sortby">
+        <option value="price-asc">Highest Price</option>
+        <option value="price-dsc">Lowest Price</option>
+        </select>
+        <div className="view">
+          <i className="fa fa-th-list" aria-hidden="true"></i>
+          <i className="fa fa-th" aria-hidden="true"></i>
+        </div>
+    </div>
+    </section>
+
+    <section className="listings-results">
+      <div className="listings">
+        <div className="listings-img">
+          <span className="Address">Address</span>
+          <div className="details">
+            <div className="user-img"></div>
+            <div className="user-details">
+              <span className="user-name">Paul of Tarsus</span>
+              <span className="post-date">10/05/2017</span>
+            </div>
+              <div className="listing-details">
+                <div className="floor-space">
+                <i class="fa fa-square-o"
+                aria-hidden="true"></i>
+                <span>1000 ft&sup2;</span>
+
+                </div>
+
+                <div className="bedrooms">
+                  <i className="fa fa-bed" aria-hidden="true"></i>
+                  <span> 3 bedrooms </span>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div className="bottom-info">
+          <span>$1000 / month</span>
+          <span><i className="fa fa-map-marker" aria-hidden="true"></i> Ridgewood, NY </span>
+
+        </div>
+      </div>
+
+    </section>
+
+    <section className="pagination">
+      <ul className="pagination-nums">
+        <li>Prev</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>Prev</li>
+      </ul>
+
+    </section>
 
       </section>)
   }
--- public/js/components/realEstate.js
+++ public/js/components/realEstate.js
@@ -366,7 +366,163 @@ var Listings = function (_Component) {
       return _react2.default.createElement(
         'section',
         { className: 'listings' },
-        'Listings'
+        _react2.default.createElement(
+          'section',
+          { className: 'search-area' },
+          _react2.default.createElement('input', { type: 'text', name: 'search' })
+        ),
+        _react2.default.createElement(
+          'section',
+          { className: 'sortby' },
+          _react2.default.createElement(
+            'div',
+            null,
+            '390 results found'
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'sort-options' },
+            _react2.default.createElement(
+              'select',
+              { name: 'sortby', className: 'sortby' },
+              _react2.default.createElement(
+                'option',
+                { value: 'price-asc' },
+                'Highest Price'
+              ),
+              _react2.default.createElement(
+                'option',
+                { value: 'price-dsc' },
+                'Lowest Price'
+              )
+            ),
+            _react2.default.createElement(
+              'div',
+              { className: 'view' },
+              _react2.default.createElement('i', { className: 'fa fa-th-list', 'aria-hidden': 'true' }),
+              _react2.default.createElement('i', { className: 'fa fa-th', 'aria-hidden': 'true' })
+            )
+          )
+        ),
+        _react2.default.createElement(
+          'section',
+          { className: 'listings-results' },
+          _react2.default.createElement(
+            'div',
+            { className: 'listings' },
+            _react2.default.createElement(
+              'div',
+              { className: 'listings-img' },
+              _react2.default.createElement(
+                'span',
+                { className: 'Address' },
+                'Address'
+              ),
+              _react2.default.createElement(
+                'div',
+                { className: 'details' },
+                _react2.default.createElement('div', { className: 'user-img' }),
+                _react2.default.createElement(
+                  'div',
+                  { className: 'user-details' },
+                  _react2.default.createElement(
+                    'span',
+                    { className: 'user-name' },
+                    'Paul of Tarsus'
+                  ),
+                  _react2.default.createElement(
+                    'span',
+                    { className: 'post-date' },
+                    '10/05/2017'
+                  )
+                ),
+                _react2.default.createElement(
+                  'div',
+                  { className: 'listing-details' },
+                  _react2.default.createElement(
+                    'div',
+                    { className: 'floor-space' },
+                    _react2.default.createElement('i', { 'class': 'fa fa-square-o',
+                      'aria-hidden': 'true' }),
+                    _react2.default.createElement(
+                      'span',
+                      null,
+                      '1000 ft\xB2'
+                    )
+                  ),
+                  _react2.default.createElement(
+                    'div',
+                    { className: 'bedrooms' },
+                    _react2.default.createElement('i', { className: 'fa fa-bed', 'aria-hidden': 'true' }),
+                    _react2.default.createElement(
+                      'span',
+                      null,
+                      ' 3 bedrooms '
+                    )
+                  )
+                )
+              )
+            ),
+            _react2.default.createElement(
+              'div',
+              { className: 'bottom-info' },
+              _react2.default.createElement(
+                'span',
+                null,
+                '$1000 / month'
+              ),
+              _react2.default.createElement(
+                'span',
+                null,
+                _react2.default.createElement('i', { className: 'fa fa-map-marker', 'aria-hidden': 'true' }),
+                ' Ridgewood, NY '
+              )
+            )
+          )
+        ),
+        _react2.default.createElement(
+          'section',
+          { className: 'pagination' },
+          _react2.default.createElement(
+            'ul',
+            { className: 'pagination-nums' },
+            _react2.default.createElement(
+              'li',
+              null,
+              'Prev'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              '1'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              '2'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              '3'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              '4'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              '5'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              'Prev'
+            )
+          )
+        )
       );
     }
   }]);
